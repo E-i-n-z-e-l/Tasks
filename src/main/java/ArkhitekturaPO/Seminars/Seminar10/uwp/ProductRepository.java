7b7f5497bfe1472df368f1fa7c8190c9e493ba23
Einzel <einzel0102@mail.ru>
2023-10-22 17:33:12 +0800

    "Семинар №10"

--- src/main/java/ArkhitekturaPO/Seminars/Seminar10/uwp/ProductRepository.java
+++ src/main/java/ArkhitekturaPO/Seminars/Seminar10/uwp/ProductRepository.java
@@ -3,7 +3,22 @@ package ArkhitekturaPO.Seminars.Seminar10.uwp;
 import java.util.ArrayList;
 import java.util.List;
 
-// Пример репозитория для работы с продуктами
+/**
+ Класс ProductRepository представляет собой репозиторий продуктов, который содержит список всех продуктов (products),
+ список новых продуктов (newProducts) и список удаленных продуктов (deletedProducts). Класс имеет методы для добавления
+ нового продукта (addNewProduct), удаления продукта (deleteProduct), подтверждения изменений (commit) и получения всех
+ продуктов (getAllProducts).
+
+ * Метод addNewProduct добавляет новый продукт в список newProducts.
+
+ * Метод deleteProduct удаляет продукт из списка products и добавляет его в список deletedProducts.
+
+ * Метод commit объединяет списки products, newProducts и deletedProducts и сохраняет изменения в базе данных.
+
+ * Метод getAllProducts возвращает копию списка products.
+
+
+ */
 public class ProductRepository {
     private List<Product> products = new ArrayList<>();
     private List<Product> newProducts = new ArrayList<>();
