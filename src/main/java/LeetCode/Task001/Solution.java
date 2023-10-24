8cbcf5fa6c9fd5b8774d28f81eef8057a752870f
Einzel <einzel0102@mail.ru>
2023-10-24 14:08:05 +0800

    "Решить задачу."

---
+++ src/main/java/LeetCode/Task001/Solution.java
@@ -0,0 +1,24 @@
+package LeetCode.Task001;
+
+/**
+ Дан массив целых чисел nums и целое число target. Вернуть индексы двух чисел в массиве,
+ которые в сумме дают target. Можно предположить, что каждый входной набор данных будет иметь
+ только одно решение, и нельзя использовать один и тот же элемент дважды. Ответ можно вернуть в
+ любом порядке.
+
+ Example 1:
+ Input: nums = [2,7,11,15], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+ Example 2:
+ Input: nums = [3,2,4], target = 6
+ Output: [1,2]
+
+ Example 3:
+ Input: nums = [3,3], target = 6
+ Output: [0,1]
+ */
+
+public class Solution {
+}
