352061289edc56cdf075149cfafa5b5066d21924
Einzel <einzel0102@mail.ru>
2023-11-03 10:56:28 +0800

    "Создание экземпляра объекта (довольно скудное)."

---
+++ src/main/java/Theory/OOP/B007_SozdaniyeEkzemplyaraObyekta/Car.java
@@ -0,0 +1,13 @@
+package Theory.OOP.B007_SozdaniyeEkzemplyaraObyekta;
+
+/**
+ Создание экземпляра объекта (довольно скудное).
+ */
+public class Car {
+    public final static String color = "Красный" ;
+    public final static String bodyType = "Купе" ;
+    public static String accelerate()
+    {
+        return "Ускоряется...";
+    }
+}
