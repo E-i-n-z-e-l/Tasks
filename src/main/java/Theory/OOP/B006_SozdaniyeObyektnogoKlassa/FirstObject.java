206ca03e8a911102825afc37130394ca1e841b7c
Einzel <einzel0102@mail.ru>
2023-11-03 10:34:43 +0800

    "Создание объектного класса/"

---
+++ src/main/java/Theory/OOP/B006_SozdaniyeObyektnogoKlassa/FirstObject.java
@@ -0,0 +1,9 @@
+package Theory.OOP.B006_SozdaniyeObyektnogoKlassa;
+
+public class FirstObject {
+    public static void main(String[] args) {
+        System.out.println( "Цвет " + Car.color ) ;
+        System.out.println( "Тип кузова " + Car.bodyType ) ;
+        System.out.println( Car.accelerate() ) ;
+    }
+}
