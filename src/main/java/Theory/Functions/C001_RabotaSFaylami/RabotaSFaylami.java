dc6e2efcde8ca037430692634eeb67a7b38d05d7
Einzel <einzel0102@mail.ru>
2023-11-14 01:47:48 +0800

    "Работа с файлами"

---
+++ src/main/java/Theory/Functions/C001_RabotaSFaylami/RabotaSFaylami.java
@@ -0,0 +1,23 @@
+package Theory.Functions.C001_RabotaSFaylami;
+
+import java.io.File;
+
+/**
+ * Обратите внимание, что при создании объекта File
+ * в действительности файл не создается, а происходит лишь представление конкретного элемента файловой системы
+ */
+public class RabotaSFaylami {
+    public static void main(String[] args) {
+        File dir = new File( "data" ) ;
+        if (dir.exists()) { // Проверяем существует ли файл.
+            String[] files = dir.list(); // Эта строка создает массив строк, содержащий имена файлов и
+                                        // подкаталогов в каталоге, представленном объектом "dir".
+            System.out.println(files.length + " файлов найдено...");
+            for (int i = 0; i < files.length; i++) {
+                System.out.println(files[i]);
+            }
+        } else {
+            System.out.println("Каталог не найден.");
+        }
+    }
+}
