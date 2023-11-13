c4cc1a287a509e427f97007dde20aaeb6e02a1a2
Einzel <einzel0102@mail.ru>
2023-11-14 01:51:56 +0800

    "Работа с файлами"

--- src/main/java/Theory/Functions/C001_RabotaSFaylami/RabotaSFaylami.java
+++ src/main/java/Theory/Functions/C001_RabotaSFaylami/RabotaSFaylami.java
@@ -1,17 +1,20 @@
 package Theory.Functions.C001_RabotaSFaylami;
 
 import java.io.File;
-
+import java.io.IOException;
 /**
  * Обратите внимание, что при создании объекта File
  * в действительности файл не создается, а происходит лишь представление конкретного элемента файловой системы
  */
 public class RabotaSFaylami {
-    public static void main(String[] args) {
-        File dir = new File("data");
+    public static void main(String[] args) throws IOException {
+        File dir = new File("data.txt");
+
+        // boolean created = dir.createNewFile(); А ВОТ ТУТ УЖЕ СОЗДАЕТСЯ ФАЙЛ.
+
         if (dir.exists()) { // Проверяем существует ли файл.
             String[] files = dir.list(); // Эта строка создает массив строк, содержащий имена файлов и
-            // подкаталогов в каталоге, представленном объектом "dir".
+                                        // подкаталогов в каталоге, представленном объектом "dir".
             System.out.println(files.length + " файлов найдено...");
             for (int i = 0; i < files.length; i++) {
                 System.out.println(files[i]);
