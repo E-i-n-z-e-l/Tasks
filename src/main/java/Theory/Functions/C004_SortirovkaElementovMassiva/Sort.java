15d47704d7222bcb6815b17cac7b4a94defc8ed5
Einzel <einzel0102@mail.ru>
2023-11-17 12:02:00 +0800

    "Сортировка элементов массива с помощью метода Arrays.sort()."

---
+++ src/main/java/Theory/Functions/C004_SortirovkaElementovMassiva/Sort.java
@@ -0,0 +1,31 @@
+package Theory.Functions.C004_SortirovkaElementovMassiva;
+
+import java.util.Arrays;
+
+/**
+ Сортировка элементов массива с помощью метода Arrays.sort().
+ */
+public class Sort {
+    public static void main(String[] args) {
+        String[] names = {"Майк", "Дэйв", "Энди"};
+        int[] nums = {200, 300, 100};
+        display(names);
+        display(nums);
+        Arrays.sort(names);
+        Arrays.sort(nums);
+        display(names);
+        display(nums);
+    }
+
+    public static void display(String[] elems) {
+        System.out.println("\nСтроковый Массив:");
+        for (int i = 0; i < elems.length; i++)
+            System.out.println("Элемент " + i + " — " + elems[i]);
+    }
+
+    public static void display(int[] elems) {
+        System.out.println("\nЦелочисленный Массив:");
+        for (int i = 0; i < elems.length; i++)
+            System.out.println("Элемент " + i + " — " + elems[i]);
+    }
+}
