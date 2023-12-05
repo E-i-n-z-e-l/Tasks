6de531b83a03c44a25cd30458349e86af56b93c8
Einzel <einzel0102@mail.ru>
2023-12-05 15:47:12 +0800

    "Напишите программу, которая будет принимать на вход строку и подсчитывать количество каждого символа в ней, используя HashMap для хранения символов и их количества."

---
+++ src/main/java/Theory/A04_SpiskiIMapu/D003_Map/Task02.java
@@ -0,0 +1,20 @@
+package Theory.A04_SpiskiIMapu.D003_Map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Напишите программу, которая будет принимать на вход строку и подсчитывать количество каждого символа в ней,
+ * используя HashMap для хранения символов и их количества.
+ */
+public class Task02 {
+    public static void main(String[] args) {
+        String word = "паралалиндром";
+        System.out.println(countingLetters(word));
+    }
+    public static HashMap<Integer, String> countingLetters (String word) {
+        HashMap<Integer, String> countingLetters = new HashMap<>();
+
+        return countingLetters;
+    }
+}
