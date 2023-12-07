ddaa45c7320a885d75851a0f142b094b8a7724b5
Einzel <einzel0102@mail.ru>
2023-12-07 11:50:51 +0800

    "Задание №4. Создайте HashMap, в котором ключами будут являться названия месяцев, а значениями - количество дней в каждом месяце. Добавьте все месяцы и их количество дней в HashMap, затем выведите все месяцы и их количество дней."

---
+++ src/main/java/Theory/A04_SpiskiIMapu/D003_Map/Task05.java
@@ -0,0 +1,21 @@
+package Theory.A04_SpiskiIMapu.D003_Map;
+
+import java.util.Scanner;
+
+/**
+ * Задание №5. Напишите программу, которая принимает на вход массив целых чисел и подсчитывает количество
+ * вхождений каждого числа в массиве, используя HashMap. Затем выведите каждое число и его количество
+ * вхождений в массиве.
+ */
+public class Task05 {
+    public static void main(String[] args) {
+
+    }
+    public static void inputArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число для записи его в массив: ");
+
+    }
+
+}
+
