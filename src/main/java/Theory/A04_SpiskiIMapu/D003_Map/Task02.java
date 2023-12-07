6cb32863b3629fd38f84bb4e7ded952a61e96386
Einzel <einzel0102@mail.ru>
2023-12-07 10:33:06 +0800

    "Задача №2. Напишите программу, которая будет принимать на вход строку и подсчитывать количество каждого символа в ней, используя HashMap для хранения символов и их количества."

--- src/main/java/Theory/A04_SpiskiIMapu/D003_Map/Task02.java
+++ src/main/java/Theory/A04_SpiskiIMapu/D003_Map/Task02.java
@@ -1,7 +1,9 @@
 package Theory.A04_SpiskiIMapu.D003_Map;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Задача №2. Напишите программу, которая будет принимать на вход строку и подсчитывать количество каждого символа в ней,
@@ -9,15 +11,24 @@ import java.util.HashMap;
  */
 public class Task02 {
     public static void main(String[] args) {
-        String word = "параллелограмм";
-        System.out.println(countingLetters(word));
+        String inputString = "программирование";
+        countingLetters(inputString);
     }
-    public static HashMap<Integer, String> countingLetters (String word) {
-        HashMap<Integer, String> countingLetters = new HashMap<>();
-
-        return countingLetters;
+    public static Map<Character, Integer> countingLetters (String word) {
+        char[] charArray = word.toCharArray(); // Переводим строку в массив символов;
+        Map<Character, Integer> charCountMap = new HashMap<>(); // Создаем новый HashMap;
+        for (char c : charArray) {
+            if (charCountMap.containsKey(c)) { // Проверяем на наличие такого ключа в HashMap.
+                charCountMap.put(c, charCountMap.get(c) + 1); // если символ уже присутствует в HashMap charCountMap
+                                                             // как ключ, то увеличивается значение этого ключа на 1
+            } else {
+                charCountMap.put(c, 1); // Если символ еще не присутствует в HashMap charCountMap, то он добавляется
+                                       // как новый ключ со значением 1
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {  // Цикл для вывода информации в терминал;
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+        return charCountMap;
     }
 }
-
-
-// ! Переделай строку в массив символов, а массив символов в массив строк(в каждой строке одна буква).
